1a0ecdf541d79bf027593e4e7d66a7a32b57d168
ggorska <ggorska.a@gmail.com>
2016-12-04 19:02:28 +0100

    Basic sender structure, notifiaction type enum, future implementation classes Email and Hangouts Sender

---
+++ src/main/java/pl/edu/agh/to/mosti/notifier/EmailSender.java
@@ -0,0 +1,8 @@
+package pl.edu.agh.to.mosti.notifier;
+
+public class EmailSender extends NotificationSender {
+    @Override
+    void sendNotification(String message, String address) {
+
+    }
+}
---
+++ src/main/java/pl/edu/agh/to/mosti/notifier/HangoutsSender.java
@@ -0,0 +1,8 @@
+package pl.edu.agh.to.mosti.notifier;
+
+public class HangoutsSender extends NotificationSender {
+    @Override
+    void sendNotification(String message, String address) {
+
+    }
+}
