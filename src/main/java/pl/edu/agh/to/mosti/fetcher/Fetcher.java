6eb4f43230c5c6f4e3649139fcd69637dca612a7
SaskoMateusz <mateuszsasko@gmail.com>
2017-01-19 18:01:23 +0100

    new fetcher structure

--- src/main/java/pl/edu/agh/to/mosti/fetcher/Fetcher.java
+++ src/main/java/pl/edu/agh/to/mosti/fetcher/Fetcher.java
@@ -1,8 +1,5 @@
 package pl.edu.agh.to.mosti.fetcher;
 
-import java.io.IOException;
-
-abstract class Fetcher {
-
-    public abstract void fetch(PageData pageData) throws IOException, InterruptedException;
+interface Fetcher {
+    FetchResult fetch(FetchRequest fetchRequest) throws FetcherException;
 }
