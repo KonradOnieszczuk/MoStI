64c39c8b95a2c3cac06d74087750da9cd8ba3ff3
SaskoMateusz <mateuszsasko@gmail.com>
2017-01-19 17:59:22 +0100

    new fetcher structure

--- src/main/java/pl/edu/agh/to/mosti/fetcher/UserDataProvider.java
+++ src/main/java/pl/edu/agh/to/mosti/fetcher/UserDataProvider.java
@@ -1,39 +1,65 @@
 package pl.edu.agh.to.mosti.fetcher;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Scanner;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import pl.edu.agh.to.mosti.comparator.Comparator;
+import pl.edu.agh.to.mosti.comparator.model.Section;
+import pl.edu.agh.to.mosti.comparator.SectionService;
+import pl.edu.agh.to.mosti.notifier.Notifier;
+import pl.edu.agh.to.mosti.notifier.NotifierInjector;
 
+@Component
+@Transactional
 public class UserDataProvider {
 
-    //pobranie danych
+    private SectionService sectionService;
+    private Comparator comparator;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private IntervalController controller = new IntervalController();
+    private Decider decider = new Decider();
 
-        Scanner scan = new Scanner(System.in);
-        boolean add = true;
-        List<String> lista = new ArrayList<String>();
-        Fetcher fetcher = new FetchStatic();
+    @Autowired
+    public UserDataProvider(SectionService sectionService, Comparator comparator) {
+        this.sectionService = sectionService;
+        this.comparator = comparator;
 
-        System.out.print("Please enter the URL: ");
-        String URL = scan.nextLine();
-
-        while(add == true){
-            System.out.print("Please enter the selector: ");
-            lista.add(scan.nextLine());
-            System.out.print("Do you want to add more selectors? [yes/no]");
-            String add_case = scan.nextLine();
-            if(add_case.equals("no")){
-                add = false;
-            }
-        }
-
-        System.out.print("Please enter the refresh time(seconds): ");
-        int Wait = scan.nextInt();
-
-        PageData page = new PageData(URL, lista, Wait);
-        fetcher.fetch(page);
+        this.decider = new Decider();
+        this.decider.register( new StaticFetcher() );
+        this.decider.register( new DynamicFetcher() );
     }
 
+    @Scheduled(fixedRate=200)
+    public void ProcessFetch(){
+
+        List <Section> sections = sectionService.getAllSections();
+
+        for ( final Section section : sections ) {
+
+            if ( !controller.contains(section.getId()) || controller.elapsed( section.getId(), section.getInterval() ))
+            {
+                FetchRequest request = new FetchRequest();
+                request.setURL( section.getUrl() );
+                request.setSelector( section.getSelector() );
+                request.setInterval( section.getInterval() );
+
+                try {
+
+                    FetchResult result = decider.decide( request );
+
+                    comparator.compare( section.getId(), result.getText() );
+
+                    controller.setCheckpoint( section.getId() );
+
+                } catch (FetcherException e) {
+                    System.out.println("Decider exception: " + e.getMessage() );
+                }
+            }
+        }
+    }
 }
