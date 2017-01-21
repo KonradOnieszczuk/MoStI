3da31bd835f9dd3f14ef58ad3a830230c7a4f00b
SaskoMateusz <mateuszsasko@gmail.com>
2017-01-22 00:37:55 +0100

    Removing dependency to comparator from FetcherContext.

--- src/main/java/pl/edu/agh/to/mosti/fetcher/FetcherContext.java
+++ src/main/java/pl/edu/agh/to/mosti/fetcher/FetcherContext.java
@@ -1,22 +1,13 @@
 package pl.edu.agh.to.mosti.fetcher;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import pl.edu.agh.to.mosti.comparator.Comparator;
-import pl.edu.agh.to.mosti.notifier.Notifier;
-import pl.edu.agh.to.mosti.notifier.NotifierInjector;
 
 @Configuration
 public class FetcherContext {
 
     @Autowired
-    public FetcherContext( Comparator comparator, UserDataProvider provider ) {
-
-        // TODO: this should be moved to notifier
-        Injector injector = Guice.createInjector(new NotifierInjector());
-        comparator.setNotifier( injector.getInstance(Notifier.class) );
+    public FetcherContext( UserDataProvider provider ) {
 
         // Configure  provider
         Decider decider = new ChainDecider();
