523494445c628fdee3a6774f2dff717487f7a7f8
SaskoMateusz <mateuszsasko@gmail.com>
2017-01-22 00:10:31 +0100

    Scheduling enabled.

--- src/main/java/pl/edu/agh/to/mosti/Application.java
+++ src/main/java/pl/edu/agh/to/mosti/Application.java
@@ -2,7 +2,9 @@ package pl.edu.agh.to.mosti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
